1a366022c5bd22c2faf8487f171316d2c54c72fa
Mahedi hasan <mahedi2112p@gmail.com>
2022-04-13 06:10:36 +0600

    implement login and redirect to shop component

---
+++ src/components/_auth_steps.js
@@ -0,0 +1,19 @@
+/* 
+1..create a new firebase project in comsole.google.com
+2..npm install firebase
+3..creat firebase.init.js import getAuth to export auth
+4..Firebase settings > Authentication Email and password auth
+5..Create Login , Signup component, setup route
+6..attach form field handler and form submit handler
+7..npm install --save react-firebase-hooks
+8..useCreateUserWithEmailAndPassword from ract-firebase-hooks 
+*/
+
+
+/* 
+// hosting steps
+1..npm install -g firebase-tools
+2..firebase login
+3..firebase init
+4..firebase deploy
+*/
