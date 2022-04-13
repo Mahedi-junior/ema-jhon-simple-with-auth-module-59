a2804f11e0416ab527c67264b54c9518708b6b4f
Mahedi hasan <mahedi2112p@gmail.com>
2022-04-13 07:15:57 +0600

    updating shipping cart

--- src/components/_auth_steps.js
+++ src/components/_auth_steps.js
@@ -7,11 +7,15 @@
 6..attach form field handler and form submit handler
 7..npm install --save react-firebase-hooks
 8..useCreateUserWithEmailAndPassword from ract-firebase-hooks 
+9..if user is created redirect to the expected page
+10..useSignInWithEmailAndPassword for Login 
+11..Create RequireAuth component ==> check user exists also track user location 
+12..in riute wrap protected component by using Require Auth Component
 */
 
 
 /* 
-// hosting steps
+//FireBase hosting steps
 1..npm install -g firebase-tools
 2..firebase login
 3..firebase init
